529937a722593e5749481e73d37335d6bdb088ed
Ilya Ivanov <ilushkaivanov@mail.ru>
2022-11-23 00:08:32 +0300

    bugfix/email-error-processing-sign-in-fix

--- routes/cards.js
+++ routes/cards.js
@@ -11,9 +11,9 @@ const {
 router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().alphanum().min(2)
+    name: Joi.string().required().min(2)
       .max(30),
-    link: Joi.string().required().alphanum()
+    link: Joi.string().required()
       .pattern(/^https?:\/\/(www.)?[A-Za-z0-9-.]+\.[a-z]+(\/[\w\-.~:/?#[\]@!$&'()*+,;=]*)?(#?)$/),
   }),
 }), createCard);
